1a799cbc107b265e771bcfae15d085c30ad70f00
Killian Perea Ruiz <killian.perea@worldline.com>
2018-02-21 16:18:29 +0100

    liferay instance

--- src/liferay-service.service.ts
+++ src/liferay-service.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 
+/* Liferay instance */
+declare const Liferay: any;
+
 @Injectable()
 export class LiferayService {
-  
-
-  constructor() {
-    // nothing to do in here :) 
-  }
 
   getUserLiferay(): string {
-    return 'cosa';
+    if (Liferay) {
+      return Liferay.ThemeDisplay.getUserName();
+    }
+    else {
+      return 'Unexpected User';
+    }
+
   }
 }
