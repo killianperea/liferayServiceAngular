08f08f282a0347837a964d39a00cc3ace7b61474
Juan Belmonte Rodríguez <reonemail@gmail.com>
2018-03-08 12:18:24 +0100

    Go back to UMD. Reduce package dependencies.

--- dist/bundle.amd.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "src/liferay-service.service" {
-    export class LiferayService {
-        getUserLiferay(): string;
-    }
-}
-declare module "index" {
-    export * from "src/liferay-service.service";
-}
